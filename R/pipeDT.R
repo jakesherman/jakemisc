6064e5378cccd3dbb18be8350aacb36fe0a15498
Jake Sherman <jake@jakesherman.com>
2015-02-16 10:36:40 -0500

    Accidentally had a ' in an roxygen2 block, causing the package not to build.

--- R/pipeDT.R
+++ R/pipeDT.R
@@ -1,18 +1,18 @@
 #' pipeDT()
 #'
-#' Evaluates "[.data.table" in a magrittr pipe-able expression.
+#' Evaluates [.data.table in a magrittr pipe-able expression.
 #' 
 #' @keywords data.table, evaluation, pipe, magrittr, data, table
 #' @param table a data.frame or data.table.
-#' @param ... arguments to be passed to "[.data.table", with the x argument
-#' to "[.data.table" already being passed by the above table argument.
+#' @param ... arguments to be passed to [.data.table, with the x argument
+#' to [.data.table already being passed by the above table argument.
 #' @param returnDT NULL by default, can set to TRUE or FALSE to explicitly 
 #' return a data.table. By default, a data.table is returned if table is a
 #' data.table, and a data.frame is returned if table is a data.frame.
 #' @export
 #' @examples
 #' 
-#' Here's an example of creating a column with dplyr, 
+#' Heres an example of creating a column with dplyr, 
 #' mtcars %<>%
 #'    mutate(Jake = cyl + 1) %T>%
 #'    pipeDT(Jake := cyl + 1)
